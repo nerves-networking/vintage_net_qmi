50b57804d4ef798d1f0325e0cababd3780d8598f
Matt Ludwigs <mattludwigs@users.noreply.github.com>
2021-03-18 14:51:06 -0700

    Release v0.1.0

--- mix.exs
+++ mix.exs
@@ -1,13 +1,23 @@
-defmodule VintageNetQmi.MixProject do
+defmodule VintageNetQMI.MixProject do
   use Mix.Project
+
+  @version "0.1.0"
+  @source_url "https://github.com/smartrent/vintage_net_qmi"
 
   def project do
     [
       app: :vintage_net_qmi,
-      version: "0.1.0",
+      version: @version,
       elixir: "~> 1.11",
       start_permanent: Mix.env() == :prod,
-      deps: deps()
+      deps: deps(),
+      docs: docs(),
+      package: package(),
+      preferred_cli_env: [
+        docs: :docs,
+        "hex.build": :docs,
+        "hex.publish": :docs
+      ]
     ]
   end
 
@@ -27,6 +37,32 @@ defmodule VintageNetQmi.MixProject do
       {:dialyxir, "~> 1.0.0", only: [:dev, :test], runtime: false},
       {:ex_doc, "~> 0.23", only: :docs, runtime: false},
       {:excoveralls, "~> 0.14", only: :test, runtime: false}
+    ]
+  end
+
+  def docs do
+    [
+      assets: "assets",
+      extras: ["README.md", "CHANGELOG.md"],
+      main: "readme",
+      source_ref: "v#{@version}",
+      source_url: @source_url,
+      skip_undefined_reference_warnings_on: ["CHANGELOG.md"]
+    ]
+  end
+
+  def package do
+    [
+      licenses: ["Proprietary"],
+      links: %{"GitHub" => @source_url},
+      files: [
+        "lib",
+        "src",
+        "CHANGELOG.md",
+        "mix.exs",
+        "README.md"
+      ],
+      organization: "smartrent"
     ]
   end
 end
