0bedf4caa3a191730ef1131004e4df3d6c3a550a
Frank Hunleth <fhunleth@troodon-software.com>
2021-04-09 16:17:45 -0400

    Add unit test

---
+++ config/config.exs
@@ -0,0 +1,12 @@
+use Mix.Config
+
+# Overrides for unit tests:
+#
+# * udhcpc_handler: capture whatever happens with udhcpc
+# * resolvconf: don't update the real resolv.conf
+# * persistence_dir: use the current directory
+config :vintage_net,
+  udhcpc_handler: VintageNetTest.CapturingUdhcpcHandler,
+  resolvconf: "/dev/null",
+  persistence_dir: "./test_tmp/persistence",
+  path: "#{File.cwd!()}/test/fixtures/root/bin"
--- test/test_helper.exs
+++ test/test_helper.exs
@@ -1,1 +1,67 @@
+defmodule Utils do
+  @moduledoc false
+
+  @spec default_opts() :: keyword()
+  def default_opts() do
+    Application.get_all_env(:vintage_net)
+  end
+
+  @spec udhcpc_child_spec(VintageNet.ifname(), String.t()) :: Supervisor.child_spec()
+  def udhcpc_child_spec(ifname, hostname) do
+    beam_notify = Application.app_dir(:beam_notify, "priv/beam_notify")
+    env = BEAMNotify.env(name: "vintage_net_comm", report_env: true)
+
+    %{
+      id: :udhcpc,
+      start: {
+        VintageNet.Interface.IfupDaemon,
+        :start_link,
+        [
+          [
+            ifname: ifname,
+            command: "udhcpc",
+            args: [
+              "-f",
+              "-i",
+              ifname,
+              "-x",
+              "hostname:#{hostname}",
+              "-s",
+              beam_notify
+            ],
+            opts: [
+              stderr_to_stdout: true,
+              log_output: :debug,
+              log_prefix: "udhcpc(#{ifname}): ",
+              env: env
+            ]
+          ]
+        ]
+      }
+    }
+  end
+
+  @spec udhcpd_child_spec(VintageNet.ifname()) :: Supervisor.child_spec()
+  def udhcpd_child_spec(ifname) do
+    env = BEAMNotify.env(name: "vintage_net_comm", report_env: true)
+
+    %{
+      id: :udhcpd,
+      restart: :permanent,
+      shutdown: 500,
+      start:
+        {MuonTrap.Daemon, :start_link,
+         [
+           "udhcpd",
+           [
+             "-f",
+             "/tmp/vintage_net/udhcpd.conf.#{ifname}"
+           ],
+           [stderr_to_stdout: true, log_output: :debug, env: env]
+         ]},
+      type: :worker
+    }
+  end
+end
+
 ExUnit.start()
