c5f9706750a15b96d142945e9cb92d5a9a0df7da
Frank Hunleth <fhunleth@troodon-software.com>
2021-04-09 16:17:45 -0400

    Add unit test

--- test/vintage_net_qmi_test.exs
+++ test/vintage_net_qmi_test.exs
@@ -1,8 +1,35 @@
-defmodule VintageNetQmiTest do
+defmodule VintageNetQMITest do
   use ExUnit.Case
-  doctest VintageNetQmi
+  alias VintageNet.Interface.RawConfig
 
-  test "greets the world" do
-    assert VintageNetQmi.hello() == :world
+  test "create a wired ethernet configuration" do
+    input = %{
+      type: VintageNetQMI,
+      vintage_net_qmi: %{service_provider: "super", device: "/dev/cdc-wdm0"},
+      ipv4: %{method: :dhcp},
+      hostname: "unit_test"
+    }
+
+    output = %RawConfig{
+      ifname: "wwan0",
+      type: VintageNetQMI,
+      source_config: input,
+      required_ifnames: ["wwan0"],
+      child_specs: [
+        {VintageNetQMI.Connection,
+         [ifname: "wwan0", device: "/dev/cdc-wdm0", service_provider: "super"]},
+        {VintageNetQMI.CellMonitor, [ifname: "wwan0", device: "/dev/cdc-wdm0"]},
+        {VintageNetQMI.SignalMonitor, [ifname: "wwan0", device: "/dev/cdc-wdm0"]},
+        Utils.udhcpc_child_spec("wwan0", "unit_test"),
+        {VintageNet.Interface.InternetConnectivityChecker, "wwan0"}
+      ],
+      down_cmds: [
+        {:run_ignore_errors, "ip", ["addr", "flush", "dev", "wwan0", "label", "wwan0"]},
+        {:run, "ip", ["link", "set", "wwan0", "down"]}
+      ],
+      up_cmds: [{:run, "ip", ["link", "set", "wwan0", "up"]}]
+    }
+
+    assert output == VintageNetQMI.to_raw_config("wwan0", input, Utils.default_opts())
   end
 end
