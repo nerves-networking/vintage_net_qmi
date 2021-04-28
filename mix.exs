e589d804b77937ab489cbc1bd048173960270824
Matt Ludwigs <mattludwigs@users.noreply.github.com>
2021-04-28 10:53:38 -0700

    Update to qmi v0.3.0

--- mix.exs
+++ mix.exs
@@ -33,7 +33,7 @@ defmodule VintageNetQMI.MixProject do
   defp deps do
     [
       {:vintage_net, "~> 0.10.0"},
-      {:qmi, "~> 0.2.0", organization: "smartrent"},
+      {:qmi, "~> 0.3.0", organization: "smartrent"},
       {:credo, "~> 1.5", only: [:dev, :test], runtime: false},
       {:dialyxir, "~> 1.0.0", only: [:dev, :test], runtime: false},
       {:ex_doc, "~> 0.23", only: :docs, runtime: false},
