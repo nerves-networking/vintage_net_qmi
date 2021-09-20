b2e45705747d4a118c35c1c558d1679bdb415c83
Matt Ludwigs <mattludwigs@users.noreply.github.com>
2021-09-20 14:17:21 -0700

    Add band, channel, access technology properties

--- lib/vintage_net_qmi/signal_monitor.ex
+++ lib/vintage_net_qmi/signal_monitor.ex
@@ -7,6 +7,8 @@ defmodule VintageNetQMI.SignalMonitor do
   alias VintageNet.PropertyTable
   alias VintageNetQMI.ASUCalculator
 
+  require Logger
+
   @type opt() :: {:ifname, binary()} | {:interval, non_neg_integer()}
 
   @spec start_link([opt()]) :: GenServer.on_start()
@@ -19,7 +21,7 @@ defmodule VintageNetQMI.SignalMonitor do
     interval = Keyword.get(args, :interval, 30_000)
     ifname = Keyword.fetch!(args, :ifname)
 
-    Process.send_after(self(), :signal_check, interval)
+    send_msgs([:signal_check, :band_and_channel_check], interval)
 
     state = %{
       ifname: ifname,
@@ -34,7 +36,21 @@ defmodule VintageNetQMI.SignalMonitor do
   def handle_info(:signal_check, state) do
     :ok = get_signal_stats(state)
 
-    Process.send_after(self(), :signal_check, state.interval)
+    send_msgs([:signal_check], state.interval)
+
+    {:noreply, state}
+  end
+
+  def handle_info(:band_and_channel_check, state) do
+    case NetworkAccess.get_rf_band_info(state.qmi) do
+      {:ok, [band_and_channel]} ->
+        post_band_and_channel_info(band_and_channel, state)
+        send_msgs([:band_and_channel_check], state.interval * 2)
+
+      {:error, _reason} ->
+        Logger.debug("[VintageNetQMI] unable to get band and channel information, retrying.")
+        send_msgs([:band_and_channel_check], state.interval)
+    end
 
     {:noreply, state}
   end
@@ -52,8 +68,25 @@ defmodule VintageNetQMI.SignalMonitor do
   end
 
   defp post_signal_rssi(%{asu: asu, dbm: dbm, bars: bars}, ifname) do
-    PropertyTable.put(VintageNet, ["interface", ifname, "mobile", "signal_asu"], asu)
-    PropertyTable.put(VintageNet, ["interface", ifname, "mobile", "signal_dbm"], dbm)
-    PropertyTable.put(VintageNet, ["interface", ifname, "mobile", "signal_4bars"], bars)
+    post_property(ifname, "signal_asu", asu)
+    post_property(ifname, "signal_dbm", dbm)
+    post_property(ifname, "signal_4bars", bars)
+  end
+
+  defp post_band_and_channel_info(
+         %{band: band, channel: channel, interface: access_technology},
+         state
+       ) do
+    post_property(state.ifname, "band", band)
+    post_property(state.ifname, "channel", channel)
+    post_property(state.ifname, "access_technology", access_technology)
+  end
+
+  defp post_property(ifname, prop_name, prop_value) do
+    PropertyTable.put(VintageNet, ["interface", ifname, "mobile", prop_name], prop_value)
+  end
+
+  defp send_msgs(messages, interval) do
+    Enum.each(messages, &Process.send_after(self(), &1, interval))
   end
 end
