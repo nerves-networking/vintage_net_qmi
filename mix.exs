b8de0f9cadc85308b237fb2917c8f27435ffd73d
Frank Hunleth <fhunleth@troodon-software.com>
2024-07-24 17:24:34 -0400

    Require Elixir 1.13 and update CI versions

--- mix.exs
+++ mix.exs
@@ -8,7 +8,7 @@ defmodule VintageNetQMI.MixProject do
     [
       app: :vintage_net_qmi,
       version: @version,
-      elixir: "~> 1.11",
+      elixir: "~> 1.13",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
       description: description(),
